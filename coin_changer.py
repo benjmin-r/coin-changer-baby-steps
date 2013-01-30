f62816159d99e3e3638ac7584c06817101005b03
Benjamin Reitzammer <benjamin@ambestengestern.de>
2013-01-30 20:42:15 +0100

    test #1

--- coin_changer.py
+++ coin_changer.py
@@ -1,8 +1,15 @@
 import unittest
 
+def coin_change(amount, coins):
+    return coins
+
 class TestCoinChanger(unittest.TestCase):
     def test_pass(self):
         pass
+
+    def test_one(self):
+        self.assertEquals(coin_change(1, [1]), [1])
+
 
 if __name__ == '__main__':
     unittest.main()
