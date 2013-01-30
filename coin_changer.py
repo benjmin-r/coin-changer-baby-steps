eda079909973580db0bd3cbfc9aa695382fad47f
Markus Tacker <m@tckr.cc>
2013-01-30 20:50:07 +0100

    Revert 2.

--- coin_changer.py
+++ coin_changer.py
@@ -1,8 +1,6 @@
 import unittest
 
 def coin_change(amount, coins):
-    if amount != reduce(lambda c1, c2: c1 + c2, coins):
-        return [amount / reduce(lambda c1, c2: c1 + c2, coins)]
     return coins
 
 class TestCoinChanger(unittest.TestCase):
@@ -11,9 +9,6 @@ class TestCoinChanger(unittest.TestCase):
 
     def test_one(self):
         self.assertEquals(coin_change(1, [1]), [1])
-
-    def test_two(self):
-        self.assertEquals(coin_change(100, [50]), [2])
 
 
 if __name__ == '__main__':
